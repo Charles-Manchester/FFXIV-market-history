9b277410036b191ade9144630c21a624349b6227
Charles-Manchester <charlz.manchester@gmail.com>
2019-02-01 17:25:52 -0800

    more cleaning

--- MarketHistory.py
+++ MarketHistory.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from filepath import desktop, laptop
 list_size = 19
 
-# Create new CSV for item and write the headers + first 20 transactions
+# Create new CSV for item and write the headers + last 20 sales
 def new_sheet(item):
     print('Generating New Sheet for ' + item)
     print('---------------------------------------------')
@@ -24,8 +24,7 @@ def new_sheet(item):
                 add_transaction(transaction, data, write_file)
     print()
     
-# Write (new) transaction data to ongoing CSV file
-# Test if any of our grabbed transactions are at a later date than our most current one in csv, and adds them to the csv
+# Update CSV for specified item with any new item sales
 def update_sheet(item):
     print('Updating History Sheet for ' + item)
     print('---------------------------------------------')
@@ -48,7 +47,7 @@ def update_sheet(item):
 
 #def update_gsheet(item):
 
-# Return: JSON transaction data for input item
+# Return: JSON transaction data for specified item
 def get_data(item):
     temp_item = item.lower()
     file_path = r'{0}\{1}_history.json'.format(laptop, temp_item)
@@ -58,36 +57,26 @@ def get_data(item):
     with open(file_path, 'w+') as write_file:
         json.dump(data, write_file)
     return data;
-    
+
+# Write a single transaction to specified CSV file
 def add_transaction(number, data, write_file):
-        fnames = ['Purchase Date - POSIX', 'Purchase Date - Day', 'Price Per Unit', 'Quantity', 'Price Total', 'Character Name']
-        marketwriter = csv.DictWriter(write_file, fieldnames=fnames)  
-        print('Transaction:', number+1, 'is new!')	
-        print('Price Per Unit:', data['PricePerUnit'])
-        print('Price Total:', data['PriceTotal'],)
-        print('Quantity:', data['Quantity'],)
-        print('Purchase Date:', data['PurchaseDate'],)
-        print('Character Name:', data['CharacterName'], '\n')
-        date_time = int(data['PurchaseDate'])
-        local_timezone = tzlocal.get_localzone()
-        local_time = datetime.fromtimestamp(date_time, local_timezone)
-        marketwriter.writerow({'Purchase Date - POSIX' : data['PurchaseDate'], 'Purchase Date - Day' : local_time.strftime('%m/%d/%Y'), 'Price Per Unit' : data['PricePerUnit'], 'Quantity' : data['Quantity'], 'Price Total' : data['PriceTotal'], 'Character Name': data['CharacterName']})
+    fnames = ['Purchase Date - POSIX', 'Purchase Date - Day', 'Price Per Unit', 'Quantity', 'Price Total', 'Character Name']
+    marketwriter = csv.DictWriter(write_file, fieldnames=fnames)  
+    print('Transaction:', number+1, 'is new!')	
+    print('Price Per Unit:', data['PricePerUnit'])
+    print('Price Total:', data['PriceTotal'],)
+    print('Quantity:', data['Quantity'],)
+    print('Purchase Date:', data['PurchaseDate'],)
+    print('Character Name:', data['CharacterName'], '\n')
+    date_time = int(data['PurchaseDate'])
+    local_timezone = tzlocal.get_localzone()
+    local_time = datetime.fromtimestamp(date_time, local_timezone)
+    marketwriter.writerow({'Purchase Date - POSIX' : data['PurchaseDate'], 'Purchase Date - Day' : local_time.strftime('%m/%d/%Y'), 'Price Per Unit' : data['PricePerUnit'], 'Quantity' : data['Quantity'], 'Price Total' : data['PriceTotal'], 'Character Name': data['CharacterName']})
             
 def main():
     for key in item_dict.keys():
         update_sheet(key)
         #update_gsheet(key)
-
-    # print('Running UNIX Time Conversion Test On First Transaction', '\n')
-    # dict = b_data['History'][0]
-    # test_time = int(dict['PurchaseDate'])
-    # print('POSIX Time:', test_time)
-    # local_timezone = tzlocal.get_localzone()
-    # local_time = datetime.fromtimestamp(test_time, local_timezone)
-
-    # print('Local Time (Date):', local_time.strftime('%m/%d/%Y'))
-    # print('Local Time (Time):', local_time.strftime('%H:%M:%S'))
-
     print('\nProgram Exit')
 	
 if __name__ == '__main__':
