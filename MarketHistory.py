7803b8bc0d5dff677696a53c39f3cee9f47531cb
Charles-Manchester <charlz.manchester@gmail.com>
2019-02-01 20:13:15 -0800

    google sheets works
    now auto-updates last 20 transaction averages on google sheets

--- MarketHistory.py
+++ MarketHistory.py
@@ -2,11 +2,14 @@ import json
 import requests
 import tzlocal
 import csv
+import pygsheets
+from SheetAverager import stats_twenty
 from itemdict import item_dict
 from apikey import api_key
 from datetime import datetime
 from filepath import desktop, laptop
 list_size = 19
+transactions = 21
 
 # Create new CSV for item and write the headers + last 20 sales
 def new_sheet(item):
@@ -14,7 +17,7 @@ def new_sheet(item):
     print('---------------------------------------------')
     temp_item = item.lower()
     json_data = get_data(temp_item)
-    file_path = r'{0}\{1}_history.csv'.format(laptop, temp_item)
+    file_path = r'{0}\Data\{1}_history.csv'.format(laptop, temp_item)
     with open(file_path, 'w+', newline='') as write_file:
         with write_file:
             fnames = ['Purchase Date - POSIX', 'Purchase Date - Day', 'Price Per Unit', 'Quantity', 'Price Total', 'Character Name']
@@ -30,7 +33,7 @@ def update_sheet(item):
     print('---------------------------------------------')
     temp_item = item.lower()  
     json_data = get_data(temp_item)
-    file_path = r'{0}\{1}_history.csv'.format(laptop, temp_item)
+    file_path = r'{0}\Data\{1}_history.csv'.format(laptop, temp_item)
     
     with open (file_path,'r' ) as read_file:
         list = read_file.readlines()
@@ -45,12 +48,19 @@ def update_sheet(item):
                     print('No New Transactions')
     print()
 
-#def update_gsheet(item):
+def update_gsheet(item):
+    average = stats_twenty(item)
+    credential = pygsheets.authorize(service_file=r'{0}\editor_credentials.json'.format(laptop))
+    sheet = credential.open('Market Comparison')
+    worksheet = sheet[0] 
+    cell = worksheet.find(item)
+    write_cell = cell[0].neighbour((0,2))
+    write_cell.value = average
 
 # Return: JSON transaction data for specified item
 def get_data(item):
     temp_item = item.lower()
-    file_path = r'{0}\{1}_history.json'.format(laptop, temp_item)
+    file_path = r'{0}\Data\{1}_history.json'.format(laptop, temp_item)
     json_request = 'https://xivapi.com/market/adamantoise/items/{0}/history?key={1}&columns=History.*19.PricePerUnit,History.*19.PriceTotal,History.*19.PurchaseDate,History.*19.Quantity,History.*19.CharacterName'.format(item_dict[temp_item], api_key)
     response = requests.get(json_request)
     data = response.json()
@@ -76,7 +86,7 @@ def add_transaction(number, data, write_file):
 def main():
     for key in item_dict.keys():
         update_sheet(key)
-        #update_gsheet(key)
+        update_gsheet(key)
     print('\nProgram Exit')
 	
 if __name__ == '__main__':
--- SheetAverager.py
+++ SheetAverager.py
@@ -1,29 +1,24 @@
 import tzlocal
 import csv
 import pygsheets
-#import sqlite3
 from filepath import desktop, laptop
 from statistics import mode, median
+transactions = 21
 
+# Return: number of lines in an item's CSV file
 def count_lines(item):
     temp_item = item
-    f_path = r'{0}\{1}_history.csv'.format(desktop, temp_item)
+    f_path = r'{0}\Data\{1}_history.csv'.format(laptop, temp_item)
 
     with open(f_path) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
                 if line_count == 0:
-                    #print(f'Column names are {", ".join(row)}')
                     line_count += 1
                 else:
-                    #print(f'\t Posix Time: {row[0]} Date: {row[1]} PPU: {row[2]} Quantity: {row[3]} Total: {row[4]} Name: {row[5]}')
-                    #total_quantity = total_quantity + int(row[3])
                     line_count += 1
-            #print(f'Processed {line_count} lines.')
             return line_count
-            #print(f'Total Quantity sold:{total_quantity}')
-            #print(f'Average Quantity sold per transaction:{total_quantity/line_count}')
 
 def stats_twenty(item):
     total_quantity = 0
@@ -31,31 +26,36 @@ def stats_twenty(item):
     twenty_list = []
     temp_item = item
     lines = count_lines(item)
-    f_path = r'{0}\{1}_history.csv'.format(desktop, 'borax')
+    f_path = r'{0}\Data\{1}_history.csv'.format(laptop, item)
     with open(f_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         parsed_data = list(csv_reader)
-        for x in range(1, 21):
-            #print(parsed_data[lines-x][3] + f' Count: {x}')
-            total_quantity = total_quantity + int(parsed_data[lines-x][3])
-            twenty_list.append(int(parsed_data[lines-x][3]))
+        for x in range(1, transactions):
+            #total_quantity = total_quantity + int(parsed_data[lines-x][3])
+            #twenty_list.append(int(parsed_data[lines-x][3]))
             total_cost = total_cost + int(parsed_data[lines-x][2])
-        print(f'Average Quantity over last 20: {total_quantity/20}')
+        #print(f'Average Quantity over last 20: {total_quantity/20}')
         print(f'Average Price over last 20: {total_cost/20}')
-        print(f'Mode over last 20: {mode(twenty_list)}')
-        print(f'Median over last 20: {median(twenty_list)}')
-
-    gc = pygsheets.authorize(service_file=r'{0}\editor_credentials.json'.format(desktop))
-    #open the google spreadsheet 
-    sh = gc.open('Market Comparison')
-    wks = sh[0]
-    #update the first sheet with df, starting at cell B2. 
-    print("Writing Data to Sheet")
-    wks.update_value('C2',total_cost/20)
+        #print(f'Mode over last 20: {mode(twenty_list)}')
+        #print(f'Median over last 20: {median(twenty_list)}')
+        print()
+    return total_cost/20
+    
+    # gc = pygsheets.authorize(service_file=r'{0}\editor_credentials.json'.format(laptop))
+    # sh = gc.open('Market Comparison')
+    # wks = sh[0]
+    # print("Writing Data to Sheet")
+    # wks.update_value('C2',total_cost/20)
     print("Program Exit")
 
 def main():
-    stats_twenty('borax')
+    average = stats_twenty('borax')
+    credential = pygsheets.authorize(service_file=r'{0}\editor_credentials.json'.format(laptop))
+    sheet = credential.open('Market Comparison')
+    worksheet = sheet[0] 
+    cell = worksheet.find("borax")
+    write_cell = cell[0].neighbour((0,2))
+    write_cell.value = average
 
 
 
